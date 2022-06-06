673aefdd2f2f16653759d2ae9a491fba3cd2f639
frank ferreira <frxnco@protonmail.com>
2022-06-06 06:27:22 -0300

    Add sections and images

--- main.js
+++ main.js
@@ -4,11 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { randFloat } from 'three/src/math/MathUtils';
 
 // Setup
-
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
@@ -17,27 +14,19 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
-
 renderer.render(scene, camera);
 
-
 // Lights
-
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
-
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-
 // const lightHelper = new THREE.PointLightHelper(pointLight)
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
-
 // const controls = new OrbitControls(camera, renderer.domElement);
-
-
 // Background
 // Animation Loop
 function render() {
@@ -60,11 +49,9 @@ function resize(renderer) {
   return needResize;
 }
 
-
 window.addEventListener( 'resize', function() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize( window.innerWidth, window.innerHeight )
 })
 
@@ -72,11 +59,11 @@ function randomColor(){
   let color = [0x50c878, 0x7850c8, 0xc850a0, 0xc87850, 0xc85064, 0xffffff]
   return color[Math.floor(Math.random()*color.length)];
 }
+
 function addStar() {
   const geometry = new THREE.SphereGeometry(randFloat(0.01,0.18), 20, 20);
   const material = new THREE.MeshStandardMaterial({ color: randomColor()});
   const star = new THREE.Mesh(geometry, material);
-
   const [x, y, z] = Array(3)
     .fill()
     .map(() => THREE.MathUtils.randFloatSpread(100));
@@ -88,25 +75,18 @@ function addStar() {
 Array(2000).fill().forEach(addStar);
 
 // Torus
-
 const geometry = new THREE.TorusGeometry(20, 1, 5, 60);
 const material = new THREE.MeshStandardMaterial({ color: 0x66cc66 });
 const torus = new THREE.Mesh(geometry, material);
 torus.position.setX(0);
-
 scene.add(torus);
 
-
 // Avatar
-
 const franTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/smeraldgreen/frank.spacev2/master/fran.png');
-
 const fran = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: franTexture }));
-
 scene.add(fran);
 
 // Moon
-
 const moonTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/smeraldgreen/frank.spacev2/master/moon.jpg');
 const marsTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/smeraldgreen/frank.spacev2/master/mars.jpg');
 const fordTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/smeraldgreen/frank.spacev2/master/ford.jpg');
@@ -140,22 +120,21 @@ scene.add(moon);
 scene.add(mars);
 scene.add(ford);
 
+//Avatar Fran
 fran.position.z = -6;
 fran.position.setY(-2);
 fran.position.setX(0);
-
+//Planet Ford
 ford.position.z = -1;
 ford.position.setX(-1);
-
+//Moon
 moon.position.z = 7;
 moon.position.setX(-3);
-
+//Planet Marte
 mars.position.z = 15;
 mars.position.setX(-5);
 
-
 // Scroll Animation
-
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
   moon.rotation.x += 0.01;
@@ -180,10 +159,6 @@ function moveCamera() {
 
 document.body.onscroll = moveCamera;
 moveCamera();
-
-
-
-
 
 function animate() {
   requestAnimationFrame(animate);
